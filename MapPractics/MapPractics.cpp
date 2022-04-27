8e1c038c16b0021f929aa323b91ab9702f57b22b
k414bin <k414bin@yandex.ru>
2022-04-27 22:08:07 +0300

    Add std::map working

--- MapPractics/MapPractics.cpp
+++ MapPractics/MapPractics.cpp
@@ -2,9 +2,21 @@
 #include <string>
 #include <iostream>
 
-
+using namespace std;
 int main()
 {
-
-    std::cout << "Hello World!\n";
+    std::map  <string, string> myDictionary;
+    myDictionary.insert(pair<string, string>("69-70-30", "Ivanov"));
+    myDictionary.insert(pair<string, string>("69-70-31", "Petrov"));
+    myDictionary.insert(pair<string, string>("69-70-32", "Petrova"));
+    myDictionary.insert(pair<string, string>("69-70-30", "Ivanova"));
+    myDictionary.insert(pair<string, string>("69-70-35", "Smirnoff"));
+    myDictionary.insert(pair<string, string>("69-70-33", "Soloviev"));
+    myDictionary.insert(pair<string, string>("69-70-34", "Vasiliev"));
+    for (auto pair : myDictionary) {
+        cout << pair.first << " " << pair.second << " \n";
+    }
+    std::cout << "\n";
+    system("pause");
+    return 0;
 }
